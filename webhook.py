489c3598b0651f7800e667ee326373561d3a9f22
SwitteeFranca2-0 <switteefrankie@gmail.com>
2023-12-16 21:23:42 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,16 +1,4 @@
 from app import app
-from app import save_webhook_output_file as file
-from app.models import *
-import csv
-
-
-"""
-This function retrieves the data from the csv file and saves to the database.
-Import the function to the file where the database is configured
-"""
-
-
-
 
 
 if __name__ == '__main__':
