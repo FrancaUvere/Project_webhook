0039085539997cccad8a0f05fb44233211daab56
SwitteeFranca2-0 <switteefrankie@gmail.com>
2024-01-03 17:58:34 +0100

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -3,6 +3,7 @@ import requests
 
 collection_name = 'ClickUp Public API'  # name of collection
 api_key = ''  # api key for the POSTMAN API
+workspace_name = '' #name of workspace
 
 
 def get_collection_uid(collection_name):
@@ -14,15 +15,27 @@ def get_collection_uid(collection_name):
     for collection_info in data['collections']:
         if collection_info['name'] == collection_name:
             return collection_info['uid']
-    return 'This collection does not exist'
+    return None
 
 
+def get_workspace_id():
+    """Get workspace id from specified data"""
+    url = 'https://api.getpostman.com/workspaces'
+    headers = {'Content-Type': 'application/json', 'X-API-KEY': api_key}
+    data = requests.get(url, headers=headers)
+    data = data.json()
+    for workspace_info in data['workspaces']:
+        if workspace_info['name'] == workspace_name:
+            return workspace_info['id']
+    return None
 
-def create_collection_webhook(collection_uid):
+def create_collection_webhook(collection_uid, workspace_id):
     """Creates collection webhook using the id"""
     url = 'https://api.getpostman.com/webhooks'
+
     #not including a 'workspace id' will create a webhook for the 'My workspace'
-    headers = {'Content-Type': 'application/json', 'X-API-KEY': api_key}
+    params = {'workspace': workspace_id}
+    headers = {'Content-Type': 'application/json', 'X-API-KEY': api_key, params: params}
     # running a POST request to the url
     webhook_name = 'ClickUp Webhook'  # random name for webhook
     data = {
@@ -44,4 +57,5 @@ def create_collection_webhook(collection_uid):
 
 
 collection_id = get_collection_uid(collection_name)
-print(create_collection_webhook(collection_id))
+workspace_id = get_workspace_id()
+print(create_collection_webhook(collection_id, workspace_id))
